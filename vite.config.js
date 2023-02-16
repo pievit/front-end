603519d557c3489d0b62d4a039cf08997b75fe45
pie.vit <pietro.vitale@gmail.com>
2023-02-16 19:19:25 +0100

    vite.config.js modifica

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ import vue2Jsx from "@vitejs/plugin-vue2-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  optimizeDeps: {
+    include: ["*"]
+  },
   plugins: [
     vue2(),
     vue2Jsx(),
