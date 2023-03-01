b358dc80b5a7a0c554adb884db5a4cf6f89b5253
pie.vit <pietro.vitale@gmail.com>
2023-03-01 12:45:22 +0100

    little changes 3.

--- src/router/index.js
+++ src/router/index.js
@@ -44,7 +44,7 @@ const router = new VueRouter({
 });
 
 router.beforeEach((to, from ,next ) => {
-  if(to == "/"){
+  if(to.name === "/"){
     next();
   }else{
     // eslint-disable-next-line no-undef,no-unused-vars
