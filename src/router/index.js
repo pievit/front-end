d8814caddea19d618276d8c720733a2a363c4135
pie.vit <pietro.vitale@gmail.com>
2023-03-06 12:30:27 +0100

    test stampa valori .7

--- src/router/index.js
+++ src/router/index.js
@@ -67,11 +67,15 @@ router.beforeEach((to, from, next) => {
   } else {
     axios.get("/login/utenti/getMailLogged").then((response) => {
       if (to.params.email !== null && response.data.toString() === to.params.email) {
-        if(to.name === "modificaDatiTas" && to.params.infoTas === null){
-          axios.get("/api/utenti/getAllInfoTassista/"+to.params.email).then((res) => {
-            console.log(res.data.toString())
-            next({name: to.name, params: {infoTas : res.data.toString()}})
-          })
+        if(to.name === "modificaDatiTas"){
+          if(to.params.infoTas === null){
+            axios.get("/api/utenti/getAllInfoTassista/"+to.params.email).then((res) => {
+              console.log(res.data.toString())
+              next({name: to.name, params: {infoTas : res.data.toString()}})
+            })
+          }else{
+            next(to.params);
+          }
         }else{
           next(to.params);
         }
