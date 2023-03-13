2ffe370ddcae938137eab85d07a80271b069caf5
pie.vit <pietro.vitale@gmail.com>
2023-03-13 23:21:11 +0100

    aggiustamenti grafici checkbox .2

--- src/modificaDisp.vue
+++ src/modificaDisp.vue
@@ -1,34 +1,38 @@
 <template>
-  <b-jumbotron
-    header="Modifica Disponibilità"
-    bg-variant="light"
-    border-variant="dark"
-    lead="Selezione le fascie orarie in cui sei disponibile"
-    rounded
-  >
-    <hr />
-    <div class="container-sm">
-      <div class="row">
-        <div class="col-2">
-          <select
-            id="giorno"
-            @change="caricaDispGiorno($event)"
-            class="form-control"
-          >
-            <option value="lun">lunedi</option>
-            <option value="mar">martedi</option>
-            <option value="mer">mercoledi</option>
-            <option value="gio">giovedi</option>
-            <option value="ven">venerdi</option>
-            <option value="sab">sabato</option>
-            <option value="dom">domenica</option>
-          </select>
+  <div class="container-sm">
+  <div class="row" align="center">
+    <b-jumbotron
+      header="Modifica Disponibilità"
+      bg-variant="light"
+      border-variant="dark"
+      lead="Selezione le fascie orarie in cui sei disponibile"
+      rounded
+    >
+      <hr />
+      <div class="container-sm">
+        <div class="row">
+          <div class="col-2">
+            <select
+              id="giorno"
+              @change="caricaDispGiorno($event)"
+              class="form-control"
+            >
+              <option value="lun">lunedi</option>
+              <option value="mar">martedi</option>
+              <option value="mer">mercoledi</option>
+              <option value="gio">giovedi</option>
+              <option value="ven">venerdi</option>
+              <option value="sab">sabato</option>
+              <option value="dom">domenica</option>
+            </select>
+          </div>
+          <div class="col-10" id="divcheck"></div>
         </div>
-        <div class="col-10" id="divcheck"></div>
       </div>
-    </div>
-    <br />
-  </b-jumbotron>
+      <br />
+    </b-jumbotron>
+  </div>
+  </div>
 </template>
 
 <script>
@@ -53,6 +57,8 @@ export default {
         o1 = (i - 1) / 2;
         o2 = ":30";
       }
+      if(o1<10)
+        o1 = "0"+o1
       label.innerHTML = o1 + o2;
       divcheck.append(label);
       if ((i + 1) % 12 === 0 && i !== 0) {
